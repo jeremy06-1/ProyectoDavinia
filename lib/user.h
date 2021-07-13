0f14857970afe68e4daa1195214ae8cb4d2ad6db
jeremy06-1 <jeremy.fonseca120@est.unanleon.edu.ni>
2021-07-12 21:02:03 -0600

    Agenda v1.9

--- lib/user.h
+++ lib/user.h
@@ -2,6 +2,7 @@
 	En este fichero se comprueba la existencia del ususario, si no existe,
 entonces lo crea
 */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,22 +10,6 @@ entonces lo crea
 #define TECLA_ENTER 13
 #define TECLA_BACKSPACE 8
 #define LONGITUD 10
-
-#if defined(_WIN32) || defined(_WIN64)
-#include <conio.h>
-// Código especial para Linux
-#elif defined(__linux__)
-void requestAndHidePasswdLnxFunc(void) {
-	
-}
-#endif
-
-// Declaración de los prototipos de las funciones
-void	chekUserExistenceFunc(void),	
-		createUserFunc(void),
-		requestAndHidePasswdWinFunc(void),
-		checkPasswdWinFunc(void),
-		printInFileFunc(void);		
 
 // Variables globales
 char usuario[LONGITUD + 1];
@@ -34,46 +19,9 @@ char caracter;
 int i=0, registrado = 0;
 FILE *username, *passwd;
 
-// Comprueba que el usuario no exista para continuar
-void chekUserExistenceFunc(void) {
-	char _char[2];
-	// Abre los ficheros correspondientes
-	username = fopen("./ficheros/username.txt", "r");
-	passwd = fopen("./ficheros/passwd.txt", "r");
-	// Por defecto los ficheros almacenan un cero lo cual significa que no existen usuario aún
-	_char[0] = fgetc(username);
-	_char[1] = fgetc(passwd);
-	
-	if (_char[0] == '0' || _char[1] == '0')	createUserFunc(); // Crea el usuario
-	
-	fclose(username);
-	fclose(passwd);
-	return;
-}
-// Crea el usuario
-void createUserFunc() {
-	/* Pedro Alfonso*/
-	printf("Bienvenido\nIngrese el nombre el nuevo usuario\n\n\tNOMBRE: ");
-	fflush(stdin);
-	gets(usuario);
-
-	printf("\tCLAVE: ");
-	// Detectar OS soportado (Windows y Linux)
-	if (strcmp(OS, "Windows") == 0) {
-		// Pedir clave y ocultarla (Windows)
-		requestAndHidePasswdWinFunc();
-	} else if (strcmp(OS, "Linux") == 0) {
-		// Pedir clave y ocultarla (Linux)
-		//requestAndHidePasswdLnxFunc();
-	} else {
-		printf("Error: Sistema operativo no soportado.");
-		exit(EXIT_FAILURE);
-	}
-	
-	checkPasswdWinFunc(); // Comprobar clave ingresada
-	printInFileFunc();	// Guardar usuario en un fiechero
-	return;
-}
+#if defined(_WIN32) || defined(_WIN64)
+// Código especial para Windows
+#include <conio.h>
 // Pide la clave para el usuario
 void requestAndHidePasswdWinFunc(void) {
 	while ((caracter = getch())) {
@@ -134,12 +82,108 @@ void checkPasswdWinFunc(void) {
 			i=0;
 			comprobante = 1;
 		} else {
-			printf("\n\nUsuario registrado correctamente\n\n");
-			system("sleep 1.5");
-			clearScreen();
 			registrado = 1;
 		}
 	} while(registrado == 0);
+	return;
+}
+#elif defined(__linux__)
+// Código especial para Linux
+#include <termios.h>
+#include <unistd.h>
+#define ECHOFLAGS (ECHO | ECHOE | ECHOK | ECHONL)
+int set_disp_mode(int fd, int option){
+	struct termios term;  
+	tcgetattr(fd, &term);
+	
+	if (option) term.c_lflag|=ECHOFLAGS;  
+	else term.c_lflag &=~ECHOFLAGS;  
+	
+	tcsetattr(fd, TCSAFLUSH, &term);  
+	return 0;
+}
+int getpasswd(char* passwd, int size) {
+	int c, n = 0;  
+
+	do {
+		c = getchar();  
+		if (c != '\n' || c != '\r') { 
+			passwd[n++] = c;
+		}
+	} while (c != '\n' && c !='\r' && n < (size - 1));  
+	passwd[n] = '\0';  
+	return n;
+} 
+void requestAndHidePasswdLnxFunc(void) {
+	getchar();
+	set_disp_mode(STDIN_FILENO,0);
+	
+	getpasswd(clave, sizeof(clave));
+	set_disp_mode(STDIN_FILENO,1);  
+	return;
+}
+#endif
+
+// Declaración de los prototipos de las funciones
+void	chekUserExistenceFunc(void),	
+		createUserFunc(void),
+		requestAndHidePasswdWinFunc(void),
+		checkPasswdWinFunc(void),
+		printInFileFunc(void);		
+
+// Comprueba que el usuario no exista para continuar
+void chekUserExistenceFunc(void) {
+	char _char[2];
+	// Abre los ficheros correspondientes
+	username = fopen("./ficheros/username.txt", "r");
+	passwd = fopen("./ficheros/passwd.txt", "r");
+	// Por defecto los ficheros almacenan un cero lo cual significa que no existen usuario aún
+	_char[0] = fgetc(username);
+	_char[1] = fgetc(passwd);
+	
+	if (_char[0] == '0' || _char[1] == '0')	createUserFunc(); // Crea el usuario
+	
+	fclose(username);
+	fclose(passwd);
+	return;
+}
+// Crea el usuario
+void createUserFunc() {
+	/* Pedro Alfonso*/
+	printf("Bienvenido\nIngrese el nombre el nuevo usuario\n\n\tNOMBRE: ");
+	fflush(stdin);
+	scanf("%s", usuario);
+
+	printf("\tCLAVE: ");
+	// Detectar OS soportado (Windows y Linux)
+	if (strcmp(OS, "Windows") == 0) {
+		// Pedir clave y ocultarla (Windows)
+		requestAndHidePasswdWinFunc();
+	} else if (strcmp(OS, "Linux") == 0) {
+		// Pedir clave y ocultarla (Linux)
+		requestAndHidePasswdLnxFunc();
+	} else {
+		printf("Error: Sistema operativo no soportado.");
+		exit(EXIT_FAILURE);
+	}
+	printf("\n\nUsuario registrado correctamente\n\n");
+	system("sleep 1.5");
+	clearScreen();
+	fflush(stdin);
+	
+	if (strcmp(OS, "Windows") == 0) {
+		checkPasswdWinFunc(); // Comprobar clave ingresada
+	} else if (strcmp(OS, "Linux") == 0) {
+		// Pedir clave y ocultarla (Linux)
+		//requestAndHidePasswdLnxFunc();
+	} else {
+		printf("Error: Sistema operativo no soportado.");
+		exit(EXIT_FAILURE);
+	}
+	
+	
+	
+	printInFileFunc();	// Guardar usuario en un fiechero
 	return;
 }
 // Guarda los datos en los ficheros
